29432f2e2c1ff49d0a5b661e09ebaf71f1424491
hrenly <hrenly@gmail.com>
2019-09-11 11:08:32 +0300

    Menu fixes

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,17 +1,50 @@
 <template>
     <v-toolbar flat>
-        <v-toolbar-items>
+        <v-toolbar-items  v-if="!$vuetify.breakpoint.smAndUp">
+            <v-menu offset-y>
+                <template v-slot:activator="{ on }">
+                    <v-btn
+                        v-on="on"
+                        text
+                    >Меню</v-btn>
+                </template>
+                <v-list>
+                    <v-list-item>
+                        <v-btn
+                            to="/"
+                            text
+                        >Список пользователей</v-btn>
+                    </v-list-item>
+                    <v-list-item>
+                        <!-- <router-link to="/add">
+                        Добавить пользователя
+                        </router-link> -->
+                        <v-btn
+                            to="/add"
+                            text
+                        >Добавить пользователя</v-btn>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-btn
+                            @click="logout"
+                            text
+                        >Выход</v-btn>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-toolbar-items>
+        <v-toolbar-items v-if="$vuetify.breakpoint.smAndUp">
             <v-btn
-                href="/"
+                to="/"
                 text
             >Список пользователей</v-btn>
             <v-btn
-                href="/add"
+                to="/add"
                 text
             >Добавить пользователя</v-btn>
         </v-toolbar-items>
-        <div class="flex-grow-1"></div>
-        <v-toolbar-items>
+        <div class="flex-grow-1" v-if="$vuetify.breakpoint.smAndUp"></div>
+        <v-toolbar-items v-if="$vuetify.breakpoint.smAndUp">
             <v-btn
                 @click="logout"
                 text
